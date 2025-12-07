8420a490123d17b3bdd2a637b1d792cfea56ff2f
Ajay Ram sasane <120932163+ajaysasane123@users.noreply.github.com>
2025-12-07 09:33:35 +0530

    fixed bcrypt railway error

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
-module.exports = (req,res,next) => {
+module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if(!authHeader) return res.status(401).json({error:'Missing token'});
+  if (!authHeader) return res.status(401).json({ error: 'Missing token' });
   const token = authHeader.split(' ')[1];
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     req.user = payload;
     next();
   } catch (err) {
-    return res.status(401).json({error:'Invalid token'});
+    return res.status(401).json({ error: 'Invalid token' });
   }
 };
